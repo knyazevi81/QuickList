3250d143ae48e90281aaf0a6ea2f944cb2b81a03
Khasan Khamraev <khasanknyazev81@gmail.com>
2023-05-11 09:04:41 +0300

    Update settings

--- bot/settings.py
+++ bot/settings.py
@@ -1,22 +1,33 @@
 import os
+import datetime
 # @mirea_ninja_search_students_bot search student
 
 # TOKENS
-TELEG_TOKEN = "6188934519:AAHJvS_EiGhMIS1R1hCrAcIlS_FaphqK9nU"  # SECRET
+TELEG_TOKEN = ""  # SECRET
 
 ADMIN_TOKEN = "revf67d88gv576e7f8r9hfwjwfj98h7r7"
 
 AUTH_TOKEN = "dv6df5687fv6df76dfv8vfd45vfd6"
 
+OPEN_AI_TOKEN = "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"
+
+
 # PATHS
 PATH_TO_DB = os.path.join(os.getcwd(), "telegram_base.db")
+
+WIND_PATH = 'cd venv/Scripts && activate && cd .. && cd .. && cd bot && python '
+
+NIX_PATH = 'source venv/bin/activate && cd bot && python '
 
 # SET
 lessons = [
     ''
 ]
 
+TIME_TO_NOTIF = "01:40"
+
 test_tokens = ["tokkjb242j23k4hb", "tokj2bh4jh23n4", "tok98yfg9w8f"]
 
 
 ADMIN_ID = 782697565
+
