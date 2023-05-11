83700494a12f1b72cd9b41965a6e40fda9333861
Khasan Khamraev <khasanknyazev81@gmail.com>
2023-05-11 09:46:45 +0300

    fix

--- bot/settings.py
+++ bot/settings.py
@@ -3,7 +3,7 @@ import datetime
 # @mirea_ninja_search_students_bot search student
 
 # TOKENS
-TELEG_TOKEN = "6188934519:AAHJvS_EiGhMIS1R1hCrAcIlS_FaphqK9nU"  # SECRET
+TELEG_TOKEN = ""  # SECRET
 
 ADMIN_TOKEN = "revf67d88gv576e7f8r9hfwjwfj98h7r7"
 
