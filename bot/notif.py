061f0605c85231bbcebe5ad081aaed2b961fa8cd
Хасан Хамраев <khasanknyazev81@gmail.com>
2023-05-12 23:51:10 +0300

    Fix: zero message

--- bot/notif.py
+++ bot/notif.py
@@ -12,20 +12,21 @@ def up_notification():
     curs = read_database.Database(settings.PATH_TO_DB)
     while True:
         telegram = notifiers.get_notifier('telegram')
-        time.sleep(5)
+        time.sleep(4)
         telegram.notify(
             token=settings.TELEG_TOKEN,
             chat_id=settings.ADMIN_ID,
             message='бот успешно работает!'
         )
-        # ДОДЕЛАТЬ УВЕДОМЛЕНИЕ
-        data_users = curs.all_user_to_notifier()
+        data_users: list = curs.all_user_to_notifier()
         for users in data_users:
-            telegram.notify(token=settings.TELEG_TOKEN,
-                            chat_id=(users[0]),
-                            message=f'❗ у вас {curs.now_tasks_num(users[0])} '
-                                    f'нерешенных задач'
-                            )
+            how_tasks: int = curs.now_tasks_num(users[0])
+            if how_tasks > 0:
+                telegram.notify(token=settings.TELEG_TOKEN,
+                                chat_id=(users[0]),
+                                message=f'❗ у вас {curs.now_tasks_num(users[0])} '
+                                        f'нерешенных задач'
+                                )
 
 
 if __name__ == '__main__':
